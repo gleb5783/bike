6b309896d959ae9e6f0597ea9444397bfed48802
gleb5783 <gleb5783@mail.ru>
2021-06-03 18:33:12 +0300

    остался скрипт

--- source/js/show-menu.js
+++ source/js/show-menu.js
@@ -2,14 +2,13 @@
 
 var ESC_BUTTON = 'Esc';
 var ESCAPE_BUTTON = 'Escape';
-var openMenuBtn = document.querySelector('#tg');
+var openMenuBtn = document.querySelector('#open-menu');
 var menu = document.querySelector('.header__list-drop-menu');
 var mainBanner = document.querySelector('.main-banner');
 
-document.body.classList.remove('body--noscroll');
-menu.classList.remove('header__list-drop-menu--nojs');
-openMenuBtn.classList.remove('header__menu-close-button');
-mainBanner.classList.remove('main-banner--nojs');
+menu.classList.toggle('header__list-drop-menu--nojs');
+openMenuBtn.classList.toggle('header__menu-close-button');
+mainBanner.classList.toggle('main-banner--nojs');
 
 function isEscEvent(evt) {
   return evt.key === ESCAPE_BUTTON || evt.key === ESC_BUTTON;
@@ -18,15 +17,16 @@ function isEscEvent(evt) {
 function closeEscMenu(evt) {
   if (isEscEvent(evt) && mainBanner.classList.value === 'main-banner main-banner--nojs') {
     openMenu();
+    document.removeEventListener('keydown', closeEscMenu);
   }
 }
 
 function openMenu() {
-  document.addEventListener('keydown', closeEscMenu);
-  document.body.classList.toggle('body--noscroll');
   openMenuBtn.classList.toggle('header__menu-close-button');
   mainBanner.classList.toggle('main-banner--nojs');
   menu.classList.toggle('header__list-drop-menu--nojs');
+  document.addEventListener('keydown', closeEscMenu);
+  document.body.classList.toggle('body--noscroll');
 }
 
 openMenuBtn.addEventListener('click', openMenu);
