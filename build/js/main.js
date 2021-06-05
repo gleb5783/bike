d9392d566af7a87353a1a78c73de10c0da7cc81f
gleb5783 <gleb5783@mail.ru>
2021-06-05 13:22:49 +0300

    правки

--- build/js/main.js
+++ build/js/main.js
@@ -5,17 +5,17 @@
   var openMenuBtn = document.querySelector('#open-menu');
   var menu = document.querySelector('.header__list-drop-menu');
   var headerLogo = document.querySelector('.header__link-logo--nojs');
-  var mainBanner = document.querySelector('.main-banner');
+  var mainBanner = document.querySelector('.main-content');
   var anchors = document.querySelectorAll('a[href*="#"]');
 
   menu.classList.remove('header__list-drop-menu--nojs');
   headerLogo.classList.remove('header__link-logo--nojs');
-  mainBanner.classList.remove('main-banner--nojs');
+  mainBanner.classList.remove('main-content--nojs');
   openMenuBtn.classList.remove('header__button--nojs');
   openMenuBtn.classList.add('header__menu-close-button', 'header__menu-open-button');
   menu.classList.toggle('header__list-drop-menu--open');
   openMenuBtn.classList.toggle('header__menu-close-button');
-  mainBanner.classList.toggle('main-banner--open-mob-menu');
+  mainBanner.classList.toggle('main-content--open-mob-menu');
 
   anchors.forEach(function (anchor) {
     anchor.addEventListener('click', function (e) {
@@ -35,14 +35,14 @@
   }
 
   function closeEscMenu(evt) {
-    if (isEscEvent(evt) && mainBanner.classList.value === 'main-banner main-banner--open-mob-menu') {
+    if (isEscEvent(evt) && mainBanner.classList.value === 'main-content main-content--open-mob-menu') {
       openMenu();
       document.removeEventListener('click', closeClickMenu);
     }
   }
 
   function closeClickMenu(evt) {
-    if (evt.target.classList.value === '' || evt.target.classList.value === 'main-banner__info-heading' || evt.target.classList.value === 'main-banner__info' || evt.target.classList.value === 'main-banner__info-text' || evt.target.classList.value === 'main-banner__form') {
+    if (evt.target.classList.value === '' || evt.target.classList.value === 'main-content__info-heading' || evt.target.classList.value === 'main-content__info' || evt.target.classList.value === 'main-content__info-text' || evt.target.classList.value === 'main-content__form') {
       openMenu();
       document.removeEventListener('click', closeClickMenu);
     }
@@ -50,12 +50,12 @@
 
   function openMenu() {
     openMenuBtn.classList.toggle('header__menu-close-button');
-    mainBanner.classList.toggle('main-banner--open-mob-menu');
+    mainBanner.classList.toggle('main-content--open-mob-menu');
     menu.classList.toggle('header__list-drop-menu--open');
     document.body.classList.toggle('body--noscroll');
     document.addEventListener('keydown', closeEscMenu);
     document.addEventListener('click', closeClickMenu);
-    if (mainBanner.classList.value === 'main-banner') {
+    if (mainBanner.classList.value === 'main-content') {
       document.body.classList.remove('body--noscroll');
     }
   }
