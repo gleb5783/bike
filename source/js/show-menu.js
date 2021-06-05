6ca1268369b37bec64591044c92de01dde4b6088
gleb5783 <gleb5783@mail.ru>
2021-06-05 12:10:45 +0300

    правки

--- source/js/show-menu.js
+++ source/js/show-menu.js
@@ -1,57 +1,65 @@
 'use strict';
+(function () {
+  var ESC_BUTTON = 'Esc';
+  var ESCAPE_BUTTON = 'Escape';
+  var openMenuBtn = document.querySelector('#open-menu');
+  var menu = document.querySelector('.header__list-drop-menu');
+  var headerLogo = document.querySelector('.header__link-logo--nojs');
+  var mainBanner = document.querySelector('.main-banner');
+  var anchors = document.querySelectorAll('a[href*="#"]');
 
-var ESC_BUTTON = 'Esc';
-var ESCAPE_BUTTON = 'Escape';
-var openMenuBtn = document.querySelector('#open-menu');
-var menu = document.querySelector('.header__list-drop-menu');
-var mainBanner = document.querySelector('.main-banner');
-var anchors = document.querySelectorAll('a[href*="#"]');
+  menu.classList.remove('header__list-drop-menu--nojs');
+  headerLogo.classList.remove('header__link-logo--nojs');
+  mainBanner.classList.remove('main-banner--nojs');
+  openMenuBtn.classList.remove('header__button--nojs');
+  openMenuBtn.classList.add('header__menu-close-button', 'header__menu-open-button');
+  menu.classList.toggle('header__list-drop-menu--open');
+  openMenuBtn.classList.toggle('header__menu-close-button');
+  mainBanner.classList.toggle('main-banner--open-mob-menu');
 
-menu.classList.toggle('header__list-drop-menu--nojs');
-openMenuBtn.classList.toggle('header__menu-close-button');
-mainBanner.classList.toggle('main-banner--nojs');
+  anchors.forEach(function (anchor) {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
 
-anchors.forEach(function (anchor) {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
+      var blockID = anchor.getAttribute('href').substr(1);
 
-    var blockID = anchor.getAttribute('href').substr(1);
-
-    document.getElementById(blockID).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
+      document.getElementById(blockID).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
     });
   });
-});
 
-function isEscEvent(evt) {
-  return evt.key === ESCAPE_BUTTON || evt.key === ESC_BUTTON;
-}
-
-function closeEscMenu(evt) {
-  if (isEscEvent(evt) && mainBanner.classList.value === 'main-banner main-banner--nojs') {
-    openMenu();
-    document.removeEventListener('click', closeClickMenu);
+  function isEscEvent(evt) {
+    return evt.key === ESCAPE_BUTTON || evt.key === ESC_BUTTON;
   }
-}
 
-function closeClickMenu(evt) {
-  if (evt.target.classList.value === '' || evt.target.classList.value === 'main-banner__info-heading' || evt.target.classList.value === 'main-banner__info' || evt.target.classList.value === 'main-banner__info-text' || evt.target.classList.value === 'main-banner__form') {
-    openMenu();
-    document.removeEventListener('click', closeClickMenu);
+  function closeEscMenu(evt) {
+    if (isEscEvent(evt) && mainBanner.classList.value === 'main-banner main-banner--open-mob-menu') {
+      openMenu();
+      document.removeEventListener('click', closeClickMenu);
+    }
   }
-}
 
-function openMenu() {
-  openMenuBtn.classList.toggle('header__menu-close-button');
-  mainBanner.classList.toggle('main-banner--nojs');
-  menu.classList.toggle('header__list-drop-menu--nojs');
-  document.body.classList.toggle('body--noscroll');
-  document.addEventListener('keydown', closeEscMenu);
-  document.addEventListener('click', closeClickMenu);
-  if (mainBanner.classList.value === 'main-banner') {
-    document.body.classList.remove('body--noscroll');
+  function closeClickMenu(evt) {
+    if (evt.target.classList.value === '' || evt.target.classList.value === 'main-banner__info-heading' || evt.target.classList.value === 'main-banner__info' || evt.target.classList.value === 'main-banner__info-text' || evt.target.classList.value === 'main-banner__form') {
+      openMenu();
+      document.removeEventListener('click', closeClickMenu);
+    }
   }
-}
 
-openMenuBtn.addEventListener('click', openMenu);
+  function openMenu() {
+    openMenuBtn.classList.toggle('header__menu-close-button');
+    mainBanner.classList.toggle('main-banner--open-mob-menu');
+    menu.classList.toggle('header__list-drop-menu--open');
+    document.body.classList.toggle('body--noscroll');
+    document.addEventListener('keydown', closeEscMenu);
+    document.addEventListener('click', closeClickMenu);
+    if (mainBanner.classList.value === 'main-banner') {
+      document.body.classList.remove('body--noscroll');
+    }
+  }
+
+  openMenuBtn.addEventListener('click', openMenu);
+})();
+
